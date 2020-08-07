f6bb3c6d7282028d01fa59806f76ab6c6d988f9d
Omar Abdelkader <omikader@gmail.com>
2020-08-06 17:37:28 -0700

    Add html theme options and update readthedocs.yml

--- docs/conf.py
+++ docs/conf.py
@@ -81,3 +81,14 @@ html_theme = "alabaster"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []  # ["_static"]
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# futher.
+html_theme_options = {
+    "canonical_url": "https://aiorobinhood.readthedocs.io",
+    "github_user": "omikader",
+    "github_repo": "aiorobinhood",
+    "github_button": True,
+    "github_type": "star",
+    "github_banner": True,
+}
